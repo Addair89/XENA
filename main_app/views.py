304e681214fdcdf49ea247622a17783d1fb33914
Spirit Barnes <spiritbarnes19@gmail.com>
2024-04-08 12:12:32 -0700

    Initial Views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return render(request, 'categories.html', {'categories': all_categories})
+
+def prompt(request, category_id):
+    prompts = UserPrompt.objects.filter(category=category_id)
+    category = Category.objects.get(id=category_id)
+    return render(request, 'prompt.html', {'prompts': prompts, 'category': category})
+
+def favorites_index(request):
+    if request.user.is_authenticated:
+        favorites = UserFavoriteImprovement.objects.filter(user=request.user)
+    else:
+        favorites = None
+    return render(request, 'favorites/index.html', {'favorites': favorites})
