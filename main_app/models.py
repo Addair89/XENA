5f933ed06ab6d73a0266473727efe8951dc5dc8a
JaxLevine <jacksonlevine@yahoo.com>
2024-04-10 14:15:30 -0400

    adding prompt history functionality

--- main_app/models.py
+++ main_app/models.py
@@ -3,23 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserPrompt(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.CharField(max_length=100)
-    prompt = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
 
-    def get_absolute_url(self):
-        return reverse('prompts/index.html', kwargs={'user_id': self.user.id})
-
-class PromptImprovement(models.Model):
-    improvement = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    user_prompt = models.ForeignKey(UserPrompt, on_delete=models.CASCADE, related_name='improvements')
-
-class UserFavoriteImprovement(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    improvements = models.ManyToManyField(PromptImprovement)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -28,6 +12,28 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+
+class UserPrompt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='user_prompts')
+    prompt = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('prompt')
+
+
+class PromptImprovement(models.Model):
+    improvement = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user_prompt = models.ForeignKey(UserPrompt, on_delete=models.CASCADE, related_name='improvements')
+
+
+class UserFavoriteImprovement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    improvements = models.ManyToManyField(PromptImprovement)
+
+
 class ExamplePrompt(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     text = models.TextField()    
