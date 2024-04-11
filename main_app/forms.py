56b6997a9050e1baf3bc4974b7cae8677b35c79b
Jared Addair <addairjared@gmail.com>
2024-04-11 12:17:20 -0700

    Added Styles

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class OpenAIForm(forms.Form):
-    input_text = forms.CharField(label='Input Text', widget=forms.Textarea)
+    input_text = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Enter prompt...'}))
